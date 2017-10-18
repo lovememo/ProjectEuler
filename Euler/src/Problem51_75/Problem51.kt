7eefeb01af57c557dc721b36919a23ef6b43d09d
lovememo <lovememo@foxmail.com>
2017-10-18 20:58:54 +0800

    kotlin file

--- Euler/src/Problem51_75/Problem51.kt
+++ Euler/src/Problem51_75/Problem51.kt
@@ -1,5 +1,7 @@
 package Problem51_75
 
+import util.lovememo.net.EulerUtil
+
 /**
  * Created by lovememo on 17-10-16.
  * Prime digit replacements
@@ -13,3 +15,19 @@ being the first member of this family, is the smallest prime with this property.
 Find the smallest prime which, by replacing part of the number (not necessarily adjacent digits) with the same digit,
 is part of an eight prime value family.
  */
+val symbols = arrayOf("*", "-")
+
+fun genSequence(n: Int): MutableList<String> {
+    if (1 == n) {
+        return symbols.toMutableList()
+    }
+    val retList = mutableListOf<String>()
+    symbols.forEach { x -> genSequence(n - 1).forEach { y -> retList.add(x + y) } }
+    return retList
+}
+
+fun main(args: Array<String>) {
+    EulerUtil.start()
+    genSequence(10).forEach { println(it) }
+    EulerUtil.end()
+}
